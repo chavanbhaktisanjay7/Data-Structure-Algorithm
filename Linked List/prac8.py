a3d4b60314f361a66f8e2743973d635032bf299e
Bhakti Chavan <chavanbhaktisanjay7@gmail.com>
2026-02-05 20:22:44 +0530

    Linked List Complete

--- Linked List/prac8.py
+++ Linked List/prac8.py
@@ -8,7 +8,7 @@ class Node:
 #creating nodes and assigning values
 Node1 = Node(1)
 Node2 = Node(2)
-Node3 = Node(2)
+Node3 = Node(3)
 Node4 = Node(1) 
 
 #linking nodes
@@ -19,27 +19,101 @@ Node3.next = Node4
 #head of the linked list
 head = Node1
 
-#hare and tortoise approach to check if linked list is palindrome
 def isPalindrome(head):
-    slow = head
-    fast = head
-    stack = []
+    #0 or 1 is palindrome
+    if head and head.next is None:
+        return True
     
-    while fast is not None and fast.next is not None:
-        stack.append(slow.data)
+    #find the middle of the linked list
+    slow = fast = head
+    while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
-    
-    if fast is not None:
-        slow = slow.next
-    
-    while slow is not None:
-        top = stack.pop()
-        
-        if top != slow.data:
+
+    #reverse the second half of the linkd list
+    prev = None
+    curr = slow
+    while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+
+    #compare the first half and the reversed second half
+    first = head
+    second = prev
+    while second:
+        if first.data != second.data:
             return False
-        slow = slow.next
-    
+        first = first.next
+        second = second.next
     return True
 
-#### yet to complete the code for checking palindrome in linked list
+print(isPalindrome(head))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#hare and tortoise approach to check if linked list is palindrome
+# def isPalindrome(head):
+#     slow = head
+#     fast = head
+#     stack = []
+    
+#     while fast is not None and fast.next is not None:
+#         stack.append(slow.data)
+#         slow = slow.next
+#         fast = fast.next.next
+    
+#     if fast is not None:
+#         slow = slow.next
+    
+#     while slow is not None:
+#         top = stack.pop()
+        
+#         if top != slow.data:
+#             return False
+#         slow = slow.next
+    
+#     return True
+
+
--- Linked List/prac9.py
+++ Linked List/prac9.py
@@ -11,13 +11,37 @@ Node1 = Node(10)
 Node2 = Node(20)
 Node3 = Node(30)
 Node4 = Node(40)
+Node5 = Node(50)
 
 #linking nodes
 Node1.next = Node2
 Node2.next = Node3
 Node3.next = Node4
+Node4.next = Node5
 
 #head of the linked list
 head = Node1
 
-#yet to complete the code 
+#print the linked list
+def printList(head):
+    curr = head
+    while curr:
+        print(curr.data, end = " -> ")
+        curr = curr.next
+    print("None")
+
+def reverseList(head):
+    prev = None
+    curr = head
+    while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    return prev
+
+print("Original Linked List:")
+printList(head) 
+reversed_head = reverseList(head)
+print("Reversed Linked List:")
+printList(reversed_head)
