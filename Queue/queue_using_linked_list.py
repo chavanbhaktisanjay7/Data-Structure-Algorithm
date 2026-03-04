b941bebc65c3a19af2fdc91e08815647662a60a8
Bhakti Chavan <chavanbhaktisanjay7@gmail.com>
2026-03-04 19:42:04 +0530

    Queue

---
+++ Queue/queue_using_linked_list.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue:
+
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.rear is None:
+            self.front = self.rear = new_node
+        else :
+            self.rear.next = new_node
+            self.rear = new_node
+
+    def dequeue(self):
+        if self.front is None:
+            return None
+        remove = self.front.data
+        self.front = self.front.next
+        if self.front is None:
+            self.rear = None
+        return remove
+        
+    def peek(self):
+        if self.front is None:
+            return None
+        return self.front.data
+    
+    def isEmpty(self):
+        return self.front is None
+    
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+print(q.dequeue())  # Output: 1
+print(q.peek())     # Output: 2
+print(q.isEmpty())  # Output: False
+print(q.dequeue())  # Output: 2
+print(q.dequeue())  # Output: 3
+print(q.isEmpty())  # Output: True
